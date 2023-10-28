11cd3d70e956a07cef116c4cecaee2f9fb632985
Shariful <sharifaiub15@gmail.com>
2023-10-28 05:37:17 +0200

    path parameter (11)

--- main.py
+++ main.py
@@ -6,4 +6,19 @@ app = FastAPI()
 @app.get('/') 
 # index function
 def index():
-    return "Hello, world!"
+    return {"message": "Hello, world!"}
+
+# path parameter example without type
+# @app.get('/blog/{id}')
+# def get_blog(id):
+#     return {"message": f"Blog with id {id}"}
+
+@app.get('/blog/all')
+def get_all_blogs():
+    return {"message": "All blogs provided!"}
+
+# path parameter example with type
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {"message": f"Blog with id {id}"}
+
