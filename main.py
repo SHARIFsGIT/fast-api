eb5ab4c24f16491590136cf1a7c4000bce3fc20c
Shariful <sharifaiub15@gmail.com>
2023-10-30 20:59:21 +0100

    tags adeed

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ app = FastAPI()
 
 # get method & endpoint for this index function
 @app.get('/') 
-# index function
-def index():
+def index():            # index function
     return {'message': 'Hello, world!'}
 
 # path parameter example without type
 # @app.get('/blog/{id}')
 # def get_blog(id):
-    # return {'message' : f'Blog with id {id}'}
+#     return {'message' : f'Blog with id {id}'}
 
 # @app.get('/blog/all')
 # def get_all_blogs():
@@ -29,15 +28,24 @@ def index():
 # def get_all_blogs(page = 1, page_size = 10):
 #     return {'message': f'All {page_size} blogs on page {page}'}
 
-# Query parameter example optional value
-@app.get('/blog/all')
+# Tag example
+@app.get('/blog/all', tags= ['blog'])
 def get_all_blogs(page = 1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
 
-# Query & Path parameter together example
-@app.get('/blog/{id}/comments/{comment_id}')
+@app.get('/blog/{id}/comments/{comment_id}', tags= ['blog', 'comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
     return {'message' : f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
+# Query parameter example optional value
+# @app.get('/blog/all')
+# def get_all_blogs(page = 1, page_size: Optional[int] = None):
+#     return {'message': f'All {page_size} blogs on page {page}'}
+
+# Query & Path parameter together example
+# @app.get('/blog/{id}/comments/{comment_id}')
+# def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+#     return {'message' : f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 # Enum path parameter
 class BlogType(str, Enum):
@@ -45,14 +53,14 @@ class BlogType(str, Enum):
     story = 'story'
     howto = 'howto'
 
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}', tags= ['blog'])
 def get_blog_type(type: BlogType):
     return {'message': f'Blog type {type}'}
 
 # path parameter example with type
-# @app.get('/blog/{id}')
-# def get_blog(id: int):
-#     return {'message': f'Blog with id {id}'}
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {'message': f'Blog with id {id}'}
 
 # status code example
 # @app.get('/blog/{id}', status_code=status.HTTP_404_NOT_FOUND)
@@ -63,7 +71,7 @@ def get_blog_type(type: BlogType):
 #         return {'message': f'Blog with id {id}'}
 
 # status code response example
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags= ['blog'])
 def get_blog(id: int, response: Response):
     if id > 5:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -71,4 +79,3 @@ def get_blog(id: int, response: Response):
     else:
         response.status_code = status.HTTP_200_OK
         return {'message': f'Blog with id {id}'}
-    
