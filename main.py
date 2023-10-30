3d7c8b7bde4a01317b82b82a5ba478dc7c11999e
Shariful <sharifaiub15@gmail.com>
2023-10-30 06:22:25 +0100

    status code added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
 from typing import Optional
 
@@ -13,28 +13,28 @@ def index():
 # path parameter example without type
 # @app.get('/blog/{id}')
 # def get_blog(id):
-#     return {"message": f"Blog with id {id}"}
+    # return {'message' : f'Blog with id {id}'}
 
 # @app.get('/blog/all')
 # def get_all_blogs():
 #     return {'message': 'All blogs provided!'}
 
-# Quarry parameter example dynamic value
+# Query parameter example dynamic value
 # @app.get('/blog/all')
 # def get_all_blogs(page, page_size):
 #     return {'message': f'All {page_size} blogs on page {page}'}
 
-# Quarry parameter example default value
+# Query parameter example default value
 # @app.get('/blog/all')
 # def get_all_blogs(page = 1, page_size = 10):
 #     return {'message': f'All {page_size} blogs on page {page}'}
 
-# Quarry parameter example optional value
+# Query parameter example optional value
 @app.get('/blog/all')
 def get_all_blogs(page = 1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
 
-# Quarry & Path parameter together example
+# Query & Path parameter together example
 @app.get('/blog/{id}/comments/{comment_id}')
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
     return {'message' : f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
@@ -50,6 +50,25 @@ def get_blog_type(type: BlogType):
     return {'message': f'Blog type {type}'}
 
 # path parameter example with type
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {'message': f'Blog with id {id}'}
+# @app.get('/blog/{id}')
+# def get_blog(id: int):
+#     return {'message': f'Blog with id {id}'}
+
+# status code example
+# @app.get('/blog/{id}', status_code=status.HTTP_404_NOT_FOUND)
+# def get_blog(id: int):
+#     if id > 5:
+#         return {'error' : f'Blog {id} not found!'}
+#     else:
+#         return {'message': f'Blog with id {id}'}
+
+# status code response example
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error' : f'Blog {id} not found!'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message': f'Blog with id {id}'}
+    
