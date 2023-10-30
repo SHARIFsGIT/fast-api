015510be09c1da270ac4f8f935a2d708cf153b10
Shariful <sharifaiub15@gmail.com>
2023-10-30 02:54:14 +0100

    predefined enum path added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -6,7 +7,7 @@ app = FastAPI()
 @app.get('/') 
 # index function
 def index():
-    return {"message": "Hello, world!"}
+    return {'message': 'Hello, world!'}
 
 # path parameter example without type
 # @app.get('/blog/{id}')
@@ -15,10 +16,20 @@ def index():
 
 @app.get('/blog/all')
 def get_all_blogs():
-    return {"message": "All blogs provided!"}
+    return {'message': 'All blogs provided!'}
+
+# Enum path parameter
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+    return {'message': f'Blog type {type}'}
 
 # path parameter example with type
 @app.get('/blog/{id}')
 def get_blog(id: int):
-    return {"message": f"Blog with id {id}"}
+    return {'message': f'Blog with id {id}'}
 
