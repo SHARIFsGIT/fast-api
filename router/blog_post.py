99bac64a1f089b7790575757810570d04dd06980
Shariful <sharifaiub15@gmail.com>
2023-10-31 05:54:48 +0100

    path & query param in req body

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,10 +1,22 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
 
 router = APIRouter(
     prefix= '/blog',
     tags= ['blog']
 )
 
-@router.post('/new')
-def create_blog():
-    pass
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    nb_comments: int
+    published: Optional[bool]
+
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        'id': id,
+        'data': blog,
+        'version': version
+        }
