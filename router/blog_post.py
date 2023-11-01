7ca835297ebe24b764d4fe98ba69767e7b722310
Shariful <sharifaiub15@gmail.com>
2023-11-01 23:39:20 +0100

    multiple, number, complex validators added

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,17 +1,25 @@
-from fastapi import APIRouter, Query, Body
+from typing import Optional, List, Dict
+from fastapi import APIRouter, Query, Body, Path
+from fastapi.params import Body
 from pydantic import BaseModel
-from typing import Optional
+
 
 router = APIRouter(
     prefix= '/blog',
     tags= ['blog']
 )
-
+class Image(BaseModel):
+    url: str
+    alias: str
 class BlogModel(BaseModel):
     title: str
     content: str
     nb_comments: int
     published: Optional[bool]
+    tags: List[str] = []
+    metadata: Dict[str, str] = {'key1': 'val1'}
+    image: Optional[Image] = None
+
 
 @router.post('/new/{id}')
 def create_blog(blog: BlogModel, id: int, version: int = 1):
@@ -21,22 +29,28 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'version': version
         }
 
-# Query parameter
-@router.post('/new/{id}/comment')
-def create_comment(blog: BlogModel, 
-                   id: int, 
-                   comment_id: int = Query(None, 
-                                           title= 'ID of the comment', 
-                                           description= 'Some description for comment_id',
-                                           alias= 'commentId',
-                                           deprecated= True
-                                        ),
-# Body parameter
-                    content: str = Body(..., min_length= 10, max_length= 15, regex= '^[a-z\s]*$') # ... or Ellipsis
-                ):
+# Query parameter & Body parameter
+@router.post('/new/{id}/comment/{comment_id}')
+def create_comment(blog: BlogModel, id: int, 
+        comment_title: int = Query(None, 
+        title= 'Title of the comment', 
+        description= 'Some description for comment_title',
+        alias= 'commentTitle',
+        deprecated= True
+    ),
+    content: str = Body(..., 
+        min_length= 10, 
+        max_length= 50, 
+        regex= '^[a-z\s]*$'
+        ), # ... or Ellipsis
+        v: List[str] = Query(['1.0', '1.1', '1.2']),
+        commment_id: int= Path(gt=5, le=10)
+    ):
     return{ 
         'blog': blog,
         'id': id,
-        'comment_id': comment_id,
-        'content': content
+        'comment_title': comment_title,
+        'content': content,
+        'version': v,
+        'comment_id': commment_id
     }
