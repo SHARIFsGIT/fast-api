a99b0bf85d0c857468347f23c4cd75b8495f4d18
Shariful <sharifaiub15@gmail.com>
2023-11-04 07:10:29 +0100

    read user added

--- db/db_user.py
+++ db/db_user.py
@@ -13,3 +13,12 @@ def create_user(db: Session, request: UserBase):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+# to read from database all users
+def get_all_users(db: Session):
+    return db.query(DbUser).all()
+
+# to read from database one user
+def get_user(db: Session, id: int):
+    return db.query(DbUser).filter(DbUser.id == id).first()
+# for more filter: return db.query(DbUser).filter(DbUser.id == id).filter(DbUser.email == email).first()
--- router/user.py
+++ router/user.py
@@ -3,6 +3,7 @@ from schemas import UserBase, UserDisplay
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_user
+from typing import List
 
 router = APIRouter(
     prefix='/user',
@@ -14,7 +15,15 @@ router = APIRouter(
 def create_user(request: UserBase, db: Session= Depends(get_db)):
     return db_user.create_user(db, request)
 
-# Read user
+# Read all users
+@router.get('/', response_model= List[UserDisplay])
+def get_all_users(db: Session= Depends(get_db)):
+    return db_user.get_all_users(db)
+
+# Read one user
+@router.get('/{id}', response_model= UserDisplay)
+def get_user(id: int, db: Session= Depends(get_db)):
+    return db_user.get_user(db, id)
 
 # Update user
 
