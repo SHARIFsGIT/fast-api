d81042d98d2d27435056bcadcc6fe57fdc0f3cc3
Shariful <sharifaiub15@gmail.com>
2023-11-10 08:26:21 +0100

    cookie added

--- router/product.py
+++ router/product.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Cookie, Header
 from fastapi.responses import HTMLResponse, Response
 from fastapi.responses import PlainTextResponse
 
@@ -16,16 +16,23 @@ def get_all_products():
     # return product
     # To show as a plain text
     data = " ".join(products)
-    return Response(content= data, media_type= "text/plain")
+    response = Response(content= data, media_type= "text/plain")
+    response.set_cookie(key= 'test_cookie', value='test_cookie_value')
+    return response
 
 # Headers are added
 @router.get('/withheader')
 def get_products(
     response: Response,
-    custom_header: Optional[List[str]] = Header(None)
+    custom_header: Optional[List[str]] = Header(None), test_cookie: Optional[str] = Cookie(None)
     ):
-    response.headers['custom_response_header'] = " and ".join(custom_header)
-    return products
+    if custom_header:
+        response.headers['custom_response_header'] = " and ".join(custom_header)
+    return {
+        'data': products,
+        'custom_header': custom_header,
+        'my_cookie': test_cookie
+        }
 
 @router.get('/{id}', responses= {
     200: {
